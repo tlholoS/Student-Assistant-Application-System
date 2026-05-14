08307438d678fa5f1699316ffb780136fe3bd2bc
modisana <motsoanynemodisana8@gmail.com>
2026-05-14 17:26:23 +0200

    added imports

---
+++ s_a_s/lib/views/student/applications_details_screen.dart
@@ -0,0 +1,15 @@
+/*
+* Student Numbers: 224102916, 224099548, 223095490, 224004511, 224022386, 224097774, 224052285, 224019969, 224008698, 224053964
+* Student Names: Ramahlosi PD, Bere AM, Motlhakane M, Modisana MD, Simelane LW, Freeman C, Seitshiro TT, Lewis DC, Molefi MD, Mfuphi L
+*/
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import '../../viewmodels/application_details_viewmodel.dart';
+import '../../models/application_model.dart';
+import '../../widgets/responsive_layout.dart';
+import '../../widgets/status_chip.dart';
+import '../../widgets/premium_widgets.dart';
