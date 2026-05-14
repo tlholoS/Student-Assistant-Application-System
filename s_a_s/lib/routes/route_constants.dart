876b82a48697689b61f3ed1c74cb1da17017cfc5
modisana <motsoanynemodisana8@gmail.com>
2026-05-14 14:28:46 +0200

    Added route constants file

---
+++ s_a_s/lib/routes/route_constants.dart
@@ -0,0 +1,14 @@
+/*
+* Student Numbers: 224102916, 224099548, 223095490, 224004511, 224022386, 224097774, 224052285, 224019969, 224008698, 224053964
+* Student Names: Ramahlosi PD, Bere AM, Motlhakane M, Modisana MD, Simelane LW, Freeman C, Seitshiro TT, Lewis DC, Molefi MD, Mfuphi L
+*/
+
+
+class RouteConstants {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String studentHome = '/studentHome';
+  static const String applicationForm = '/applicationForm';
+  static const String applicationDetails = '/applicationDetails';
+  static const String adminHome = '/adminHome';
+}
