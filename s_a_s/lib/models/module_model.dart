87cb9c8293cd7efbb44921b704aa01b45d558737
tlholoS <tlholos2004@gmail.com>
2026-05-14 13:26:04 +0200

    added comments

--- s_a_s/lib/models/module_model.dart
+++ s_a_s/lib/models/module_model.dart
@@ -1,3 +1,9 @@
+/*
+* Student Numbers: 224102916, 224099548, 223095490, 224004511, 224022386, 224097774, 224052285, 224019969, 224008698, 224053964
+* Student Names: Ramahlosi PD, Bere AM, Motlhakane M, Modisana MD, Simelane LW, Freeman C, Seitshiro TT, Lewis DC, Molefi MD, Mfuphi L
+
+*/
+
 class ModuleModel {
   final String id;
   final String moduleCode;
