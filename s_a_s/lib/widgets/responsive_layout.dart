165418793dbdba2abcd2dac5965a7878f508840c
Luyanda1706 <224053974@stud.cut.ac.za>
2026-05-14 19:03:52 +0200

    Added code within responsive_layout file

---
+++ s_a_s/lib/widgets/responsive_layout.dart
@@ -0,0 +1,27 @@
+/*
+* Student Numbers: 224102916, 224099548, 223095490, 224004511, 224022386, 224097774, 224052285, 224019969, 224008698, 224053964
+* Student Names: Ramahlosi PD, Bere AM, Motlhakane M, Modisana MD, Simelane LW, Freeman C, Seitshiro TT, Lewis DC, Molefi MD, Mfuphi L
+*/
+
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget child;
+  final double maxWidth;
+
+  const ResponsiveLayout({
+    super.key, 
+    required this.child,
+    this.maxWidth = 800.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: maxWidth),
+        child: child,
+      ),
+    );
+  }
+}
