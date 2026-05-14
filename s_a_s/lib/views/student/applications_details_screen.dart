fd7df5229d9ce843c7ed600eedb4da7d56ca4a61
Pheello18 <224102916@stud.ac.za>
2026-05-14 17:40:51 +0200

    added imports

--- s_a_s/lib/views/student/applications_details_screen.dart
+++ s_a_s/lib/views/student/applications_details_screen.dart
@@ -2,7 +2,6 @@
 * Student Numbers: 224102916, 224099548, 223095490, 224004511, 224022386, 224097774, 224052285, 224019969, 224008698, 224053964
 * Student Names: Ramahlosi PD, Bere AM, Motlhakane M, Modisana MD, Simelane LW, Freeman C, Seitshiro TT, Lewis DC, Molefi MD, Mfuphi L
 */
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
